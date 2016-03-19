756b68ae4be3059b7d849ca36eabab375af4c6f8
Simon Schick <demwizzy@gmail.com>
2016-03-19 16:40:52 +0100

    Applied linting, no longer defining a global but returns instead

--- lib3d2d.lua
+++ lib3d2d.lua
@@ -3,7 +3,7 @@
 		Rendering VGUI in 3D-Space
 
 	USAGE:
-	
+
 	PANEL:Set3D2D(bool)
 	PANEL:Set3D2DScale(float)
 	PANEL:Set3D2DPos(vector)
@@ -11,6 +11,8 @@
 ]]--
 
 local _R = debug.getregistry()
+
+local lib3d2d = _R.lib3d2d
 if not lib3d2d then
 	lib3d2d = {}
 
@@ -23,17 +25,18 @@ if not lib3d2d then
 	}
 	lib3d2d.fadeDistance = 1000^2--maximum render distance^2
 else
-	if lib3d2d.proxyTextEntryWrapper then
-		lib3d2d.proxyTextEntryWrapper:Remove()
-	end
+	return lib3d2d
 end
 
 --FIXME:
 --Fix Event handling
 
 function lib3d2d.enable()
+	if lib3d2d then
+		error("lib3d2d is already enabled")
+	end
 	lib3d2d.active = true
-	local NULL_ANG = Angle(0, 0, 0)--default values
+	local NULL_ANG = Angle(0, 0, 0) --default values
 	local NULL_VEC = Vector(0, 0, 0)
 
 	local activePanels =  {}
@@ -51,12 +54,12 @@ function lib3d2d.enable()
 	local currentPanelPosition = NULL_VEC
 	local currentPanelAngles = NULL_ANG
 	local currentPanelScale = 1
-	
+
 	function lib3d2d.debug()
 		hook.Add("HUDPaint", "3D2DVGUI", function()
 			surface.SetFont("default")
 			surface.SetTextColor(255, 255, 255, 255)
-			
+
 			surface.SetTextPos(20, 20)
 			surface.DrawText("viewOrigin: " .. tostring(viewOrigin))
 			surface.SetTextPos(20, 40)
@@ -77,14 +80,14 @@ function lib3d2d.enable()
 			surface.DrawText("cursorPosY: " .. tostring(cursorPosY))
 		end)
 	end
-	
-	
-	
+
+
+
 
 	--Needed to allow text input
 	local currentTextEntry
 
-	local proxyTextEntryWrapper = vgui.Create("EditablePanel")--needed for focusing the TextEntry
+	local proxyTextEntryWrapper = vgui.Create("EditablePanel") --needed for focusing the TextEntry
 
 	local proxyTextEntry = vgui.Create("DTextEntry", proxyTextEntryWrapper)
 
@@ -100,8 +103,8 @@ function lib3d2d.enable()
 		self:SetText("")
 		func(self)
 	end
-	
-	
+
+
 
 	proxyTextEntryWrapper:SetSize(100, 20)
 	proxyTextEntryWrapper:SetPos(-100, -100)
@@ -133,7 +136,7 @@ function lib3d2d.enable()
 	--Updates cursorPosX and cursorPosY for currentPanel
 	local WorldToLocal = WorldToLocal
 	local mfloor = math.floor
-	
+
 	--Inline ray trace intersection
 	local function updateCurrent3D2DCursorPos()
 		local collisionPoint = rayPlaneIntersect(
@@ -145,14 +148,14 @@ function lib3d2d.enable()
 		if not collisionPoint then
 			return
 		end
-		
+
 		local projectedPos = WorldToLocal(collisionPoint, NULL_ANG, currentPanelPosition, currentPanelAngles)
 		projectedPos:Mul(1 / currentPanelScale)
-		
+
 		cursorPosX = mfloor(projectedPos.x)
 		cursorPosY = mfloor(-projectedPos.y)
 	end
-	
+
 	local function isInRelRect(x, y, minX, minY, sizeX, sizeY)
 		return x >= minX and x <= minX + sizeX and y >= minY and y <= minY + sizeY
 	end
@@ -175,7 +178,7 @@ function lib3d2d.enable()
 		if not previousHoverState and not newHoverState then
 			return
 		end
-		
+
 		local handled = false
 		local posX, posY = pnl:LocalToScreen(0, 0)
 		local x, y
@@ -190,12 +193,10 @@ function lib3d2d.enable()
 
 		for k, child in next, pnl:GetChildren() do
 			local cx, cy = child:GetPos()
-			
-			local w, h = child:GetSize()
-			
+
 			x = posX + cx
 			y = posY + cy
-			
+
 			local hVal = handlePanelEvent(child, event, ...)
 			if hVal then
 				handled = true
@@ -207,11 +208,11 @@ function lib3d2d.enable()
 				if pnl:GetClassName() == "TextEntry" then--special handling of text entries to allow focus
 					proxyTextEntry:SetValue(pnl:GetValue())
 					proxyTextEntry:SetCaretPos(pnl:GetValue():len())
-				
+
 					currentTextEntry = pnl
-					
-					proxyTextEntryWrapper:SetVisible(true)--Needs to be visible
-					proxyTextEntryWrapper:MakePopup()--GAIN DAT FOCUS
+
+					proxyTextEntryWrapper:SetVisible(true) --Needs to be visible
+					proxyTextEntryWrapper:MakePopup() --GAIN DAT FOCUS
 					proxyTextEntry:RequestFocus()
 				end
 			end
@@ -240,7 +241,7 @@ function lib3d2d.enable()
 		end
 		return oldGuiMouseX()
 	end
-	
+
 	local oldGuiMouseY = gui.MouseY
 	lib3d2d.detours.gui.MouseY = oldGuiMouseY
 	function gui.MouseY()
@@ -249,7 +250,7 @@ function lib3d2d.enable()
 		end
 		return oldGuiMouseY()
 	end
-	
+
 	local oldGuiMousePos = gui.MousePos
 	lib3d2d.detours.gui.MousePos = oldGuiMousePos
 	function gui.MousePos()
@@ -268,7 +269,7 @@ function lib3d2d.enable()
 		end
 		return oldCursorPos(self)
 	end
-	
+
 	local oldSetCursor = _R.Panel.SetCursor
 	lib3d2d.detours._R.Panel.SetCursor = oldSetCursor
 	function _R.Panel:SetCursor(cur)
@@ -278,13 +279,10 @@ function lib3d2d.enable()
 	--EVENT HOOKING
 
 	--MOUSE CAPTURE
-	local lastHoverState
 	local panel
 	local tremove = table.remove
 	local maxDist
-	local doBreak = false
-	local x, y, w, h
-	
+
 	local cursorMats = {
 		user = Material("cursor_user.png"),
 		arrow = Material("cursor_arrow.png"),
@@ -301,15 +299,15 @@ function lib3d2d.enable()
 		no = Material("cursor_no.png"),
 		hand = Material("cursor_hand.png")
 	}
-	
+
 	hook.Add("CalcView", "3D2DVGUI", function(_, pos, ang)
 		if #activePanels ~= 0 then
 			if (viewOrigin ~= pos) or (viewDirectionAngles ~= ang) then--only update if the player changed their orientation
 				shouldDetourGuiPos = true
 				viewOrigin = pos
 				viewDirectionAngles = ang
-				viewDirection = ang:Forward()--EyeVector() doesn't work here
-				
+				viewDirection = ang:Forward() --EyeVector() doesn't work here
+
 				for i = 1, #activePanels do
 					panel = activePanels[i]
 					maxDist = panel.__3d2dFadeDistance
@@ -318,14 +316,13 @@ function lib3d2d.enable()
 						(pos.z - panel.__3d2dPos.z) ^ 2
 						) > maxDist then
 						visiblePanels[panel] = false
-						continue
 					end
 					visiblePanels[panel] = true
-					
+
 					if not panel:IsValid() then
-						tremove(activePanels, i)--drop invalid panel
+						tremove(activePanels, i) --drop invalid panel
 					end
-					
+
 					handlePanelEvent(panel, "OnCursorMoved")
 				end
 				shouldDetourGuiPos = false
@@ -340,7 +337,7 @@ function lib3d2d.enable()
 	local cEnd3D2D = cam.End3D2D
 	local rGetToneMappingScaleLinear = render.GetToneMappingScaleLinear
 	local rSetToneMappingScaleLinear = render.SetToneMappingScaleLinear
-	
+
 	local targetScale = Vector(0.66, 0 , 0)
 	local oldScale
 	hook.Add("PostDrawOpaqueRenderables", "3D2DVGUI", function()
@@ -354,15 +351,15 @@ function lib3d2d.enable()
 				continue
 			end
 			oldScale = rGetToneMappingScaleLinear()
-			
+
 			rSetToneMappingScaleLinear(targetScale)
-			
+
 			cStart3D2D(panel.__3d2dPos, panel.__3d2dAng, panel.__3d2dScale)
-			
+
 				panel:SetPaintedManually(false)
 				panel:PaintManual()
 				panel:SetPaintedManually(true)
-				
+
 				if panel.__hovered and not(panel.__cursor ~= "none" or panel.__cursor ~= "last" or
 				panel.__cursor ~= "blank") then
 					surface.SetMaterial(cursorMats[panel.__cursor])
@@ -370,7 +367,7 @@ function lib3d2d.enable()
 					surface.DrawTexturedRect(cursorPosX, cursorPosY, 16, 16)
 				end
 			cEnd3D2D()
-			
+
 			rSetToneMappingScaleLinear(oldScale)
 		end
 		shouldDetourGuiPos = false
@@ -381,7 +378,7 @@ function lib3d2d.enable()
 	local IN_RELOAD = IN_RELOAD
 	local MOUSE_LEFT = MOUSE_LEFT
 	local MOUSE_RIGHT = MOUSE_RIGHT
-	
+
 	local hCall = hook.Call
 	hook.Add("KeyPress", "3D2DVGUI", function(_, key)
 		if #activePanels ~= 0 then
@@ -389,23 +386,17 @@ function lib3d2d.enable()
 				shouldDetourGuiPos = true
 				for i = 1, #activePanels do
 					panel = activePanels[i]
-					if not visiblePanels[panel] then
+					if not visiblePanels[panel] or not panel.__hovered then
 						continue
 					end
-					if panel.__hovered then
-						if key == IN_USE then
-							handlePanelEvent(panel, "OnMousePressed", MOUSE_LEFT)
-							hCall("VGUIMousePressed", GM,  pnl, MOUSE_LEFT)
-						else
-							handlePanelEvent(panel, "OnMousePressed", MOUSE_RIGHT)
-							hCall("VGUIMousePressed", GM, pnl, MOUSE_RIGHT)
-						end
-					end
+					local mKey = key == IN_USE and MOUSE_LEFT or MOUSE_RIGHT
+					handlePanelEvent(panel, "OnMousePressed", mKey)
+					hCall("VGUIMousePressed", GM,  pnl, MOUSE_LEFT)
 				end
 				shouldDetourGuiPos = false
 			end
 		end
-	end)			
+	end)
 
 	hook.Add("KeyRelease", "3D2DVGUI", function(_, key)
 		if #activePanels ~= 0 then
@@ -413,23 +404,16 @@ function lib3d2d.enable()
 				shouldDetourGuiPos = true
 				for i = 1, #activePanels do
 					panel = activePanels[i]
-					if not visiblePanels[panel] then
+					if not visiblePanels[panel] or not panel.__hovered then
 						continue
 					end
-					
-					if panel.__hovered then
-						if key == IN_USE then
-							handlePanelEvent(panel, "OnMouseReleased", MOUSE_LEFT)
-						else
-							handlePanelEvent(panel, "OnMouseReleased", MOUSE_RIGHT)
-						end
-					end
+					handlePanelEvent(panel, "OnMouseReleased", IN_USE and MOUSE_LEFT or MOUSE_RIGHT)
 				end
 				shouldDetourGuiPos = false
 			end
 		end
 	end)
-	
+
 	--Scroll capture
 	hook.Add("PlayerBindPress", "3D2DVGUI", function(_, bind, _)
 		if #activePanels ~= 0 then
@@ -437,23 +421,18 @@ function lib3d2d.enable()
 				shouldDetourGuiPos = true
 				for i = 1, #activePanels do
 					panel = activePanels[i]
-					if not visiblePanels[panel] then
+					if not visiblePanels[panel] or not panel.__hovered then
 						continue
 					end
-					if panel.__hovered then
-						if bind == "invprev" then
-							handlePanelEvent(panel, "OnMouseWheeled", -0.5)
-						else
-							handlePanelEvent(panel, "OnMouseWheeled", 0.5)
-						end
-					end
+					local delta = bind == "invprev" and -0.5 or 0.5
+					handlePanelEvent(panel, "OnMouseWheeled", delta)
 				end
 				shouldDetourGuiPos = false
 			end
 		end
 	end)
-	
-	
+
+
 	local keyCache = {}
 	for k, v in next, _G do--CACHE DAT TABLE
 		if k:sub(0, 4) == "KEY_" then
@@ -467,30 +446,18 @@ function lib3d2d.enable()
 		if #activePanels ~= 0 then
 			for i = 1, 159 do--159 is highest key
 				keyState = iIsKeyDown(i)
-				if keyState ~= keyCache[i] then
-					if keyState then
-						for i = 1, #activePanels do
-							panel = activePanels[i]
-							if not visiblePanels[panel] then
-								continue
-							end
-							if panel.__hovered then
-								handlePanelEvent(panel, "OnKeyCodePressed", i)
-							end
-						end
-					else
-						for i = 1, #activePanels do
-							panel = activePanels[i]
-							if not visiblePanels[panel] then
-								continue
-							end
-							if panel.__hovered then
-								handlePanelEvent(panel, "OnKeyCodeReleased", i)
-							end
-						end
-					end
-					keyCache[i] = keyState
+				if keyState == keyCache[i] then
+					continue
 				end
+				local eventName = keyState and "OnKeyCodePressed" or "OnKeyCodeReleased"
+				for k = 1, #activePanels do
+					panel = activePanels[k]
+					if not visiblePanels[panel] or not panel.__hovered then
+						continue
+					end
+					handlePanelEvent(panel, eventName, k)
+				end
+				keyCache[i] = keyState
 			end
 		end
 	end)
@@ -504,18 +471,8 @@ function lib3d2d.enable()
 		shouldDetourGuiPos = false
 	end
 
-	local function tableHasValue(val)
-		for i = 1, #activePanels do
-			if val == activePanels[i] then
-				return i
-			end
-		end
-		return false
-	end
 
-
-	--methodes
-	local tremove = table.remove
+	--methods
 	local tinsert = table.insert
 	function _R.Panel:Set3D2D(b)
 		if b then
@@ -531,16 +488,16 @@ function lib3d2d.enable()
 			self.__3d2dExitEventCalled = nil
 			self.__3d2dFadeDistance = nil
 		end
-		
-		self:SetPaintedManually(b)--prevent 2d drawing
-		
+
+		self:SetPaintedManually(b) --prevent 2d drawing
+
 		local idx = false
 		for i = 1, #activePanels do
 			if val == activePanels[i] then
 				idx = i
 			end
 		end
-		
+
 		if idx and not b then
 			tremove(activePanels, idx)
 			if self.__oldThink then
@@ -558,7 +515,7 @@ function lib3d2d.enable()
 			end
 		end
 	end
-	
+
 	function _R.Panel:Set3D2DFadeDistance(distance)
 		self.__3d2dFadeDistance = distance
 	end
@@ -597,12 +554,12 @@ end
 
 function lib3d2d.disable()
 	if not lib3d2d.active then
-		return
+		error("lib3d2d not enabled")
 	end
 	lib3d2d.active = false
-	
+
 	lib3d2d.proxyTextEntryWrapper:Remove()
-	
+
 	for k, panel in next, lib3d2d.activePanels do
 		if panel.__oldThink then
 			panel.Think = panel.__oldThink
@@ -614,16 +571,16 @@ function lib3d2d.disable()
 		panel.__3d2dExitEventCalled = nil
 		panel.__3d2dFadeDistance = nil
 	end
-	
+
 	lib3d2d.activePanels = nil
-	
-	
+
+
 	--unsetting methodes
 	_R.Panel.Set3D2D = nil
 	_R.Panel.Set3D2DPos = nil
 	_R.Panel.Set3D2DAng = nil
 	_R.Panel.Set3D2DScale = nil
-	
+
 	--removing hooks
 	hook.Remove("CalcView", "3D2DVGUI")
 	hook.Remove("PostDrawOpaqueRenderables", "3D2DVGUI")
@@ -632,6 +589,8 @@ function lib3d2d.disable()
 	hook.Remove("PlayerBindPress", "3D2DVGUI")
 	hook.Remove("Think", "3D2DVGUI")
 	hook.Remove("HUDPaint", "3D2DVGUI")
-	
+
 	removeDetours(lib3d2d.detours, _G)
 end
+
+return lib3d2d
